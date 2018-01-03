ed29d17cc8ceebf0c5ba9a7b23d368a8d46122bb
arcadedelux <redredemtion101@gmail.com>
2018-01-03 13:56:49 +0700

    Latihan1

--- Lingkaran.java
+++ Lingkaran.java
@@ -1,9 +1,9 @@
+import java.lang.Math;
 public class Lingkaran{
 	public static void main(String[] args){
 	
 	Double jariJari = 15.0;
 	Double diameter = jariJari*2;
-	Double phi = 3.14;
-	System.out.println("Nilai PI =" + phi + "maka nilai kelilingnya adalah  =" + (diameter * phi) + "cm");
+	System.out.println("Nilai PI =" + Math.PI + "maka nilai kelilingnya adalah  =" + (diameter * Math.PI) + "cm");
 	}
 }
