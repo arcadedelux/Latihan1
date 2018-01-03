4c3a4138f763f8308b94ff080d7354abcc693dd9
arcadedelux <redredemtion101@gmail.com>
2018-01-03 15:00:50 +0700

    Latihan Panggil Variabel

---
+++ PanggilVar.java
@@ -0,0 +1,8 @@
+public class PanggilVar{
+	public static void main(String[] args){
+	
+	System.out.println("Maka Luas Perseginya Adalah = " + Variabel.hasilPersegi +  "cm");
+	System.out.println("Maka Luas Persegi Panjang Adalah = " + Variabel.hasilPersegiPanjang + "cm");
+	System.out.println("Maka Keliling Lingkaran Adalah =  " + Variabel.hasilLingkaran +  "cm");
+	}
+}
