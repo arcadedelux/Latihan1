22aca5a9df02fe946d1d3135279ac24f01b585c6
arcadedelux <redredemtion101@gmail.com>
2018-01-03 15:05:33 +0700

    Tambah line keterangan

--- Variabel.java
+++ Variabel.java
@@ -1,10 +1,13 @@
 import java.lang.Math;
 public class Variabel{
+	//Hitung Luas Persegi
  public static Double sisi = 50.0;
  public static Double hasilPersegi = sisi * sisi /100;
+	//Hitung Luas Persegi Panjang
  public static Integer panjang = 10;
  public static Integer tinggi =  5;
  public static Integer hasilPersegiPanjang =panjang * tinggi;
+	//Hitung Keliling Persegi
  public static Double jariJari = 15.0;
  public static Double diameter = jariJari*2;
  public static Double hasilLingkaran =diameter * Math.PI;
